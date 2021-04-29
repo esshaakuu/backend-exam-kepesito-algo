29a2d091a6741a098874bf639e342d8b5c15a7fa
esshaakuu <kutierik.hu@gmail.com>
2021-04-29 10:34:17 +0200

    done With All tests

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,21 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+    Integer counter = 0;
+
+    public Integer addDigits(String input) {
+        if(input == null || input.isEmpty()) {
+            counter--;
+        } else {
+
+            for (int i = 0; i < input.length(); i++) {
+                char c = input.charAt(i);
+                if (Character.isDigit(c)) {
+                    int a = Character.getNumericValue(c);
+                    counter = counter + a;
+                }
+            }
+        }
+        return counter;
+    }
 }
